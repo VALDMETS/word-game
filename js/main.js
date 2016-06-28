1f345928d4310e6e5069d0548f9dbdf82e95a3ac
Ben Valdmets-Harris <benvaldmets@gmail.com>
2016-06-28 18:52:46 -0500

    lookin beautiful

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+var gameActive = 1;
 var gameWords = commonWords.filter(function(word){
   return (word.length >= 3);
 });
@@ -5,38 +6,60 @@ var gameWords = commonWords.filter(function(word){
 var randMult = gameWords.length;
 var goalNum = (Math.floor(randMult*Math.random()));
 var goal = gameWords[goalNum];
-
-console.log(goal);
+var goalCheck = goal.split('');
+var letterCheck = [''];
+var cc = 0;
 
 var blanks = [''];
 for (var i = 0; i < goal.length; i++) {
-  blanks[i] = '_ ';
+  blanks[i] = ' _ ';
 }
 var blanksDisplay = blanks.join('');
-
 var deathTimer = 8;
+
 document.querySelector(".deathbox").innerText = deathTimer;
 document.querySelector(".guessbox").innerText = blanksDisplay;
 
 window.addEventListener('keyup', function(e){
   var guess = e.key;
   var change = 0;
-  for (var i = 0; i < goal.length; i++) {
-    if (goal[i] === e.key) {
-      console.log(e.key);
-      blanks[i] = e.key;
-      blanksDisplay = blanks.join('');
-      document.querySelector(".guessbox").innerText = blanksDisplay;
-      change = 1;
-    }
-  }
-  if (change === 0) {
-    if (deathTimer<=0) {
-      deathTimer = 'YOU LOSE';
-    } else if (deathTimer>0) {
-      deathTimer--;
-    }
-    document.querySelector(".deathbox").innerText = deathTimer;
-  }
 
+  if (gameActive === 1) {
+    if (letterCheck.indexOf(e.key)===-1 && e.keyCode>=65 && e.keyCode<=90) {
+      for (var i = 0; i < goal.length; i++) {
+        if (goal[i] === guess) {
+          blanks[i] = guess;
+          blanksDisplay = blanks.join('');
+          document.querySelector(".guessbox").innerText = blanksDisplay;
+          change = 1;
+        }
+      }
+      if (change === 0) {
+        if (deathTimer<=1) {
+          deathTimer = 'YOU LOSE';
+          gameActive = 0;
+          document.querySelector(".failures").innerText = 'YOU LOSE';
+          document.querySelector(".guessbox").innerText = goal;
+          document.querySelector(".tries").innerText = '';
+        } else if (deathTimer>0) {
+          deathTimer--;
+          var classcounter = 'wrong' + deathTimer;
+          document.body.classList.add(classcounter);
+        }
+        document.querySelector(".deathbox").innerText = deathTimer;
+      }
+      if (blanks.join('') === goalCheck.join('')) {
+        document.querySelector(".deathbox").innerText = 'YOU WIN!!';
+        document.querySelector(".failures").innerText = 'YOU WIN!!';
+        document.querySelector(".tries").innerText = '';
+        gameActive = 0;
+      }
+      letterCheck[cc] = e.key;
+      cc++;
+      document.querySelector('.triesbox').innerText = letterCheck.join('');
+    }
+    else {
+      document.querySelector('.triesbox').innerText = 'try a different letter!';
+    }
+  }
 });
